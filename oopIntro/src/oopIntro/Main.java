76d2edc6e878033b315e6d3ba31553f6b87407cc
yusufenesdanalioglu <yusufenes3494@gmail.com>
2021-04-25 17:01:33 +0300

    ok
    ok

--- oopIntro/src/oopIntro/Main.java
+++ oopIntro/src/oopIntro/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		Product product1 = new Product(1,"Lenova V14",15000,"16 Gb Ram");
 		System.out.println(product1.name);
-		//asdasd
+		//asdasdasd
 		
 	}
 }
