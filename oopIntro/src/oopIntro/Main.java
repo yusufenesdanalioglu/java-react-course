eca5afc253b76933a77af62d698273ccc34c01be
yedanalioglu <41891933+yedanalioglu@users.noreply.github.com>
2021-04-25 15:25:05 +0300

    oopIntro
    Second homework from Engin Demiroğ oopIntro

---
+++ oopIntro/src/oopIntro/Main.java
@@ -0,0 +1,11 @@
+package oopIntro;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Product product1 = new Product(1,"Lenova V14",15000,"16 Gb Ram");
+		System.out.println(product1.name);
+		
+		
+	}
+}
---
+++ oopIntro/src/oopIntro/Product.java
@@ -0,0 +1,22 @@
+package oopIntro;
+
+import java.lang.reflect.Constructor;
+
+public class Product {
+	public Product(){
+		System.out.println("Boþ çalýþtým");
+	}
+	
+	public Product(int id, String name, double unitPrice, String detail){
+		this.id = id;
+		this.name = name;
+		this.unitPrice = unitPrice;
+		this.detail = detail;
+	}
+	
+	int id;
+	String name;
+	double unitPrice;
+	String detail;
+	
+}
